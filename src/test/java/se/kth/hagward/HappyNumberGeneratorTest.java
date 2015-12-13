eb908f3e4fa5931149917cb7b157d6999b7bfefb
hagward <taperlawyer@gmail.com>
2015-12-13 12:30:59 +0100

    Use shorter variable names in tests

--- src/test/java/se/kth/hagward/HappyNumberGeneratorTest.java
+++ src/test/java/se/kth/hagward/HappyNumberGeneratorTest.java
@@ -20,48 +20,48 @@ public class HappyNumberGeneratorTest {
 
     @Test
     public void testIsHappy() {
-        HappyNumberGenerator happyNumberGenerator = new HappyNumberGenerator();
+        HappyNumberGenerator generator = new HappyNumberGenerator();
 
         // Check that the happy numbers are indeed happy.
         for (int happyNumber : HAPPY_NUMBERS) {
-            Assert.assertTrue(happyNumberGenerator.isHappy(happyNumber));
+            Assert.assertTrue(generator.isHappy(happyNumber));
         }
 
         // Check that the numbers in-between the happy numbers really are unhappy.
         for (int i = 0; i < HAPPY_NUMBERS.length - 1; i++) {
             for (int unhappyNumber = HAPPY_NUMBERS[i] + 1; unhappyNumber < HAPPY_NUMBERS[i + 1]; unhappyNumber++) {
-                Assert.assertFalse(happyNumberGenerator.isHappy(unhappyNumber));
+                Assert.assertFalse(generator.isHappy(unhappyNumber));
             }
         }
     }
 
     @Test
     public void testGetSquareDigitSum() {
-        HappyNumberGenerator happyNumberGenerator = new HappyNumberGenerator();
+        HappyNumberGenerator generator = new HappyNumberGenerator();
 
-        Assert.assertEquals(0, happyNumberGenerator.getSquareDigitSum(0));
-        Assert.assertEquals(1, happyNumberGenerator.getSquareDigitSum(1));
+        Assert.assertEquals(0, generator.getSquareDigitSum(0));
+        Assert.assertEquals(1, generator.getSquareDigitSum(1));
 
-        Assert.assertEquals(810, happyNumberGenerator.getSquareDigitSum(9999999999L));
+        Assert.assertEquals(810, generator.getSquareDigitSum(9999999999L));
 
         long digitSum = 1 + 4 + 9 + 16 + 25 + 36 + 49 + 64 + 81;
-        Assert.assertEquals(digitSum, happyNumberGenerator.getSquareDigitSum(123456789));
-        Assert.assertEquals(digitSum, happyNumberGenerator.getSquareDigitSum(1234567890));
-        Assert.assertEquals(digitSum, happyNumberGenerator.getSquareDigitSum(1234506789));
-        Assert.assertEquals(digitSum, happyNumberGenerator.getSquareDigitSum(123450678900000L));
-        Assert.assertEquals(digitSum, happyNumberGenerator.getSquareDigitSum(95873421600000L));
+        Assert.assertEquals(digitSum, generator.getSquareDigitSum(123456789));
+        Assert.assertEquals(digitSum, generator.getSquareDigitSum(1234567890));
+        Assert.assertEquals(digitSum, generator.getSquareDigitSum(1234506789));
+        Assert.assertEquals(digitSum, generator.getSquareDigitSum(123450678900000L));
+        Assert.assertEquals(digitSum, generator.getSquareDigitSum(95873421600000L));
     }
 
     @Test
     public void testGetHappyNumbers() {
-        HappyNumberGenerator happyNumberGenerator = new HappyNumberGenerator();
+        HappyNumberGenerator generator = new HappyNumberGenerator();
 
         // Check that we receive all happy numbers between 1 and 1000 (both inclusive).
         List<Long> happyNumbers = new ArrayList<>();
         for (int happyNumber : HAPPY_NUMBERS) {
             happyNumbers.add((long) happyNumber);
         }
-        Assert.assertEquals(happyNumbers, happyNumberGenerator.getHappyNumbers(1000));
+        Assert.assertEquals(happyNumbers, generator.getHappyNumbers(1000));
     }
 
 }
